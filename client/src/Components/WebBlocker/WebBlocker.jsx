879360cc314b7f59a68bcc457d5bbbc9dacbec18
Binay Tripathy <binaytripathy123@gmail.com>
2023-10-07 22:23:35 +0530

    Timely Commit
    Signed-off-by: Binay Tripathy <binaytripathy123@gmail.com>

--- client/src/Components/WebBlocker/WebBlocker.jsx
+++ client/src/Components/WebBlocker/WebBlocker.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './WebBlocker.scss'
 
 const WebBlocker = () => {
+
+  const [blockedSites, setBlockedSites] = useState([]);
+  const [siteToAdd, setSiteToAdd] = useState('');
+
+  const handleAddSite = () => {
+    setBlockedSites([...blockedSites, siteToAdd]);
+    setSiteToAdd('');
+  };
+
+  const handleRemoveSite = (site) => {
+    const updatedSites = blockedSites.filter((s) => s !== site);
+    setBlockedSites(updatedSites);
+  };
+
   return (
-    <div>WebBlocker</div>
-  )
+    <div>
+      <h1>Social Media Blocker</h1>
+      <input
+        type="text"
+        placeholder="Enter a social media site (e.g., facebook.com)"
+        value={siteToAdd}
+        onChange={(e) => setSiteToAdd(e.target.value)}
+      />
+      <button onClick={handleAddSite}>Add Site</button>
+      <ul>
+        {blockedSites.map((site) => (
+          <li key={site}>
+            {site} <button onClick={() => handleRemoveSite(site)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+
 }
 
 export default WebBlocker
+
