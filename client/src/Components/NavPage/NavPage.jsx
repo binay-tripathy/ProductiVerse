f8bcebb891d30bbb03790ddaf672a024ebe6f54d
Binay Tripathy <binaytripathy123@gmail.com>
2023-10-09 23:18:16 +0530

    Studied Timing stuffs
    Signed-off-by: Binay Tripathy <binaytripathy123@gmail.com>

--- client/src/Components/NavPage/NavPage.jsx
+++ client/src/Components/NavPage/NavPage.jsx
@@ -11,37 +11,16 @@ const NavPage = () => {
             <div className="nav">
                 <div className="row">
                     <Link to='/task'className="navlink"><span>Manage your tasks</span></Link>
-                    {/* <button >
-                        <a href="#/task"></a>
-                    </button> */}
                     <Link to='/playlist'className="navlink"><span>Playlist</span></Link>
-                    {/* <button className="navlink">
-                        <a href='#/playlist'></a>
-                    </button> */}
                 </div>
                 <div className="row">
                 <Link to='/pomodoro'className="navlink"><span>Pomodoro Timer</span></Link>
-
-                    {/* <button className="navlink">
-                        <a href='#/pomodoro'></a>
-                    </button> */}
                 <Link to='/blocker'className="navlink"><span>Website Blocker</span></Link>
-
-                    {/* <button className="navlink">
-                        <a href='#/blocker'></a>
-                    </button> */}
                 </div>
-                <div className="row">
+                {/* <div className="row">
                 <Link to='/analytics'className="navlink"><span>Analytics</span></Link>
-
-                    {/* <button className="navlink">
-                        <a href='#/analytics'></a>
-                    </button> */}
                 <Link to='/feedback'className="navlink"><span>Feedback and Support</span></Link>
-                    {/* <button className="navlink">
-                        <a href='#/feedback'></a>
-                    </button> */}
-                </div>
+                </div> */}
                 <div className="row2">
                     <button className="navlink2">
                         <span>Quotes</span>
